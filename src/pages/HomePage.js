349490b81ca58dc0eb1c77bf2ade439afb68838b
AsirAAlam <Alvi.Abrar29@gmail.com>
2022-09-29 00:39:13 -0400

    Basic Name, Intro, Colors

---
+++ src/pages/HomePage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {
+  Box,
+  Container
+} from '@mui/material';
+
+const PAGE_COLOR = '#F6E7DB';
+
+function HomePage() {
+  return (
+    <Box sx={{ height: '100vh', bgcolor: PAGE_COLOR }}>
+      <Box pt={30} />
+
+      <Container sx={{ width: 600, ml: '50%' }}>
+        <Box sx={{
+          fontFamily: 'Oxygen',
+          fontSize: 40,
+          fontWeight: 700,
+          color: '#784527'
+        }}>
+          Mikhail Mikhaylov
+        </Box>
+
+        <Box m={1} />
+
+        <Box sx={{
+          fontFamily: 'Belleza',
+          color: '#62361C',
+          fontSize: 20,
+        }}>
+          Hello! My name is Mikhail Mikhaylov and I am an aspiring politician,
+          lawyer, and foreign service officer. I am fond of journalism, economics,
+          and politics, and therefore, I push myself to get as involved as possible
+          with civic engagement in my community. Here's a glimpse into the journey
+          of my career! Allow me to take you on a journey through my career!
+        </Box>
+      </Container>
+
+    </Box>
+  )
+}
+
+export default HomePage;
