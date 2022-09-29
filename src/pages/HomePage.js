89058080a9de676fadac54148e782f26d1179844
AsirAAlam <Alvi.Abrar29@gmail.com>
2022-09-29 00:44:46 -0400

    fix intro msg repeated line

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,7 +8,7 @@ const PAGE_COLOR = '#F6E7DB';
 
 function HomePage() {
   return (
-    <Box sx={{ height: '100vh', bgcolor: PAGE_COLOR }}>
+    <Box sx={{ height: '200vh', bgcolor: PAGE_COLOR }}>
       <Box pt={30} />
 
       <Container sx={{ width: 600, ml: '50%' }}>
@@ -31,8 +31,7 @@ function HomePage() {
           Hello! My name is Mikhail Mikhaylov and I am an aspiring politician,
           lawyer, and foreign service officer. I am fond of journalism, economics,
           and politics, and therefore, I push myself to get as involved as possible
-          with civic engagement in my community. Here's a glimpse into the journey
-          of my career! Allow me to take you on a journey through my career!
+          with civic engagement in my community. Allow me to take you on a journey through my career!
         </Box>
       </Container>
 
