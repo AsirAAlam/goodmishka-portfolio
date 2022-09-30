d5227889cbec5da653435d18151bf3582c6629c6
AsirAAlam <Alvi.Abrar29@gmail.com>
2022-09-30 01:00:58 -0400

    Initial parallax + intial left scroll navigator

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import {
   Box,
-  Container
 } from '@mui/material';
 
-const PAGE_COLOR = '#F6E7DB';
+import './HomePageStyles.css';
 
-function HomePage() {
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+
+function useParallax(value, distance) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
+function NameIntro({ id }) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 300);
+
   return (
-    <Box sx={{ height: '200vh', bgcolor: PAGE_COLOR }}>
-      <Box pt={30} />
-
-      <Container sx={{ width: 600, ml: '50%' }}>
+    <div className="scroll-area">
+      <Box sx={{ width: 600, ml: '50%' }}>
         <Box sx={{
           fontFamily: 'Oxygen',
           fontSize: 40,
@@ -23,7 +35,7 @@ function HomePage() {
 
         <Box m={1} />
 
-        <Box sx={{
+        <motion.div style={{
           fontFamily: 'Belleza',
           color: '#62361C',
           fontSize: 20,
@@ -32,8 +44,55 @@ function HomePage() {
           lawyer, and foreign service officer. I am fond of journalism, economics,
           and politics, and therefore, I push myself to get as involved as possible
           with civic engagement in my community. Allow me to take you on a journey through my career!
-        </Box>
-      </Container>
+        </motion.div>
+      </Box>
+    </div>
+  );
+}
+
+function Item({ id }) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 300);
+
+  return (
+    <div className="scroll-area">
+      <div ref={ref}>
+        <Box sx={{ height: 500, width: 300, bgcolor: 'green' }} />
+      </div>
+      <motion.h2 style={{ y }}>{`#00${id}`}</motion.h2>
+    </div>
+  );
+}
+
+function SideNavigatorItem() {
+  return (
+    <Box sx={{ mt: 1, height: 80, bgcolor: '#DCC0AA', borderRadius: 5 }} />
+  );
+};
+
+function HomePage() {
+  return (
+    <Box className="scroll-container" sx={{ height: '100vh' }}>
+      {/* <img src={require('../MishaPortrait.png')} style={{ position: 'absolute', height: '100vh', top: 0 }}/> */}
+      {/* <Box pt={30} /> */}
+
+      <NameIntro />
+      <Item id={1} />
+      <Item id={2} />
+
+      <Box sx={{
+        width: 20,
+        transform: 'translateY(-50%)',
+        position: 'fixed',
+        top: '50%',
+        left: '20px'
+      }}>
+        <Box sx={{ height: 20, bgcolor: '#DCC0AA', borderRadius: 5 }} />
+        <SideNavigatorItem />
+        <SideNavigatorItem />
+        <SideNavigatorItem />
+      </Box>
 
     </Box>
   )
