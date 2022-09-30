c550afd3083112c3034ba46d6a57e8e08b25dd8f
AsirAAlam <Alvi.Abrar29@gmail.com>
2022-09-30 14:07:46 -0400

    add NameIntro parallax

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,7 @@ import {
 } from '@mui/material';
 
 import './HomePageStyles.css';
+import NameIntro from '../components/NameIntro';
 
 import { useRef } from "react";
 import {
@@ -14,40 +15,6 @@ import {
 
 function useParallax(value, distance) {
   return useTransform(value, [0, 1], [-distance, distance]);
-}
-
-function NameIntro() {
-  // const ref = useRef(null);
-  // const { scrollYProgress } = useScroll({ target: ref });
-  // const y = useParallax(scrollYProgress, 300);
-
-  return (
-    <div className="scroll-area">
-      <Box sx={{ width: 600, ml: '50%' }}>
-        <Box sx={{
-          fontFamily: 'Oxygen',
-          fontSize: 40,
-          fontWeight: 700,
-          color: '#784527'
-        }}>
-          Mikhail Mikhaylov
-        </Box>
-
-        <Box m={1} />
-
-        <motion.div style={{
-          fontFamily: 'Belleza',
-          color: '#62361C',
-          fontSize: 20,
-        }}>
-          Hello! My name is Mikhail Mikhaylov and I am an aspiring politician,
-          lawyer, and foreign service officer. I am fond of journalism, economics,
-          and politics, and therefore, I push myself to get as involved as possible
-          with civic engagement in my community. Allow me to take you on a journey through my career!
-        </motion.div>
-      </Box>
-    </div>
-  );
 }
 
 function Item({ id }) {
@@ -73,13 +40,17 @@ function SideNavigatorItem() {
 
 function HomePage() {
   return (
-    <Box className="scroll-container" sx={{ height: '100vh' }}>
-      {/* <img src={require('../MishaPortrait.png')} style={{ position: 'absolute', height: '100vh', top: 0 }}/> */}
-      {/* <Box pt={30} /> */}
-
+    <Box>
       <NameIntro />
-      <Item id={1} />
-      <Item id={2} />
+
+      <Box className="scroll-container" sx={{ height: '100vh' }}>
+        {/* <img src={require('../MishaPortrait.png')} style={{ position: 'absolute', height: '100vh', top: 0 }}/> */}
+        {/* <Box pt={30} /> */}
+
+        <Item id={1} />
+        <Item id={2} />
+        <Item id={3} />
+      </Box>
 
       <Box sx={{
         width: 20,
@@ -93,7 +64,6 @@ function HomePage() {
         <SideNavigatorItem />
         <SideNavigatorItem />
       </Box>
-
     </Box>
   )
 }
